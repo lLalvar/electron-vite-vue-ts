20c05ffb8f87698d2e295c27ad1aec9ebd9a2f73
lLalvar <mnacakanyan.lalvar@gmail.com>
2024-03-26 14:21:17 +0400

    Init

---
+++ electron.vite.config.ts
@@ -0,0 +1,21 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+        '@': resolve('src/renderer/src')
+      }
+    },
+    plugins: [vue()]
+  }
+})
---
+++ src/renderer/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex min-h-screen flex-col items-center justify-center">
+    <h2>Technologies Used:</h2>
+    <p>Electron, Vue, Tailwind</p>
+  </div>
+</template>
